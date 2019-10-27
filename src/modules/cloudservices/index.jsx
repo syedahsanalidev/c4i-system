92f1a070d904a3030b4026b1872deafc7f50ea09
Syed Ahsan Ali <ahsan.ali@tkxel.com>
2019-10-27 11:21:58 +0500

    Saas,Paas,Laas

--- src/modules/cloudservices/index.jsx
+++ src/modules/cloudservices/index.jsx
@@ -1,138 +1,202 @@
 import React from 'react';
 import './cloudservices.css';
-const services=()=>{
-    return   <section className="quiz_section" id="quizeSection">
+
+const services = () => {
+    const cloudServices = [
+        {
+            title: 'Saas'
+        }, {
+            title: 'Paas'
+        }, {
+            title: 'Laas'
+        },
+    ];
+    return <section className="quiz_section" id="quizeSection">
         <div className="container">
             <div className="row">
-                <div className="col-sm-12" style={{"margin-top":"10px"}}>
+                <div className="col-sm-12" style={{"margin-top": "10px"}}>
                     <div className="quiz_content_area">
                         <h1 className="quiz_title">Cloud Services</h1>
                         <div className="row">
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={1} defaultValue={1} defaultChecked="checked" />
+                                    <input className="quiz_checkbox" type="checkbox" id={1} defaultValue={1}
+                                           defaultChecked="checked"/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <div className="quiz_icon quiz_icon1" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <div className="quiz_icon quiz_icon1"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title gose Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title gose Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <div className="quiz_icon quiz_icon2" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <div className="quiz_icon quiz_icon2"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Gose Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Gose Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <div className="quiz_icon quiz_icon3" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <div className="quiz_icon quiz_icon3"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Gose Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Gose Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <div className="quiz_icon quiz_icon4" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <div className="quiz_icon quiz_icon4"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Goes Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Goes Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <i className="fa fa-home" aria-hidden="true" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <i className="fa fa-home" aria-hidden="true"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Goes Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Goes Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <i className="fa fa-telegram" aria-hidden="true" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <i className="fa fa-telegram" aria-hidden="true"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Goes Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Goes Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <i className="fa fa-coffee" aria-hidden="true" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <i className="fa fa-coffee" aria-hidden="true"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Goes Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Goes Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
                             <div className="col-sm-3">
                                 <div className="quiz_card_area">
-                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2} />
+                                    <input className="quiz_checkbox" type="checkbox" id={2} defaultValue={2}/>
                                     <div className="single_quiz_card">
                                         <div className="quiz_card_content">
                                             <div className="quiz_card_icon">
-                                                <i className="fa fa-check" aria-hidden="true" />
-                                            </div>{/* end of quiz_card_media */}
+                                                <i className="fa fa-check" aria-hidden="true"/>
+                                            </div>
+                                            {/* end of quiz_card_media */}
                                             <div className="quiz_card_title">
-                                                <h3><i className="fa fa-check" aria-hidden="true" /> Title Goes Here</h3>
-                                            </div>{/* end of quiz_card_title */}
-                                        </div>{/* end of quiz_card_content */}
-                                    </div>{/* end of single_quiz_card */}
-                                </div>{/* end of quiz_card_area */}
-                            </div>{/* end of col3  */}
-                        </div>{/* end of quiz_card_area */}
-                    </div>{/* end of quiz_content_area */}
-                </div>{/* end of col12 */}
-            </div>{/* end of row */}
-        </div>{/* end of container */}
+                                                <h3><i className="fa fa-check" aria-hidden="true"/> Title Goes Here</h3>
+                                            </div>
+                                            {/* end of quiz_card_title */}
+                                        </div>
+                                        {/* end of quiz_card_content */}
+                                    </div>
+                                    {/* end of single_quiz_card */}
+                                </div>
+                                {/* end of quiz_card_area */}
+                            </div>
+                            {/* end of col3  */}
+                        </div>
+                        {/* end of quiz_card_area */}
+                    </div>
+                    {/* end of quiz_content_area */}
+                </div>
+                {/* end of col12 */}
+            </div>
+            {/* end of row */}
+        </div>
+        {/* end of container */}
     </section>
 };
 export default services;
