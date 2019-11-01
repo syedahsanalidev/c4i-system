18b927d5a8538eabd0b3bccfab65c0619c25b06f
Syed Ahsan Ali <ahsan.ali@tkxel.com>
2019-11-01 11:24:42 +0500

    chart changes

--- src/modules/chart/index.jsx
+++ src/modules/chart/index.jsx
@@ -13,15 +13,33 @@ const Chart = () => {
     const margin = {top: 20, right: 20, bottom: 30, left: 40};
 
     const {width} = state;
-    return (<div ref={inputRef}>
-        <div style={{width: '50%'}}>
-            <BarChart ylabel='Percentage'
-                      width={width}
-                      height={500}
-                      margin={margin}
-                      data={data}/>
+    return (<section className="quiz_section" id="quizeSection">
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12" style={{marginTop: "10px"}}>
+                    <div className="quiz_content_area">
+                        <h1 className="quiz_title">Recommended Service Provider</h1>
+                        <div className="row">
+                            <div ref={inputRef}>
+                                <div style={{width: '50%'}}>
+                                    <BarChart ylabel='Percentage'
+                                              width={width}
+                                              height={500}
+                                              margin={margin}
+                                              data={data}/>
+                                </div>
+                            </div>
+                        </div>
+                        {/* end of quiz_card_area */}
+                    </div>
+                    {/* end of quiz_content_area */}
+                </div>
+                {/* end of col12 */}
+            </div>
+            {/* end of row */}
         </div>
-    </div>)
+        {/* end of container */}
+    </section>)
 };
 export default Chart;
 
